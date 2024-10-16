d883641f4d056e78f45d741a4686a5fd5b9a2567
ilkerymz <iilker0053@gmail.com>
2024-10-16 17:55:20 +0300

    Participant model created

--- ticketup-backend/src/main/java/com/codever/ticketup/model/Participant.java
+++ ticketup-backend/src/main/java/com/codever/ticketup/model/Participant.java
@@ -1,8 +1,24 @@
 package com.codever.ticketup.model;
 
-public class Participant {
-    private String firstName;
-    private String lastName;
+import jakarta.persistence.*;
+import jdk.jfr.Event;
+import lombok.*;
 
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "participants")
+public class Participant {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
 }
