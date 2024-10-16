4ee359152c8aff298d7430be45dc94288fa7e851
Abdullah <aturkmen2021@gtu.edu.tr>
2024-10-16 21:26:16 +0300

    participant has changed

--- ticketup-backend/src/main/java/com/codever/ticketup/service/ParticipantService.java
+++ ticketup-backend/src/main/java/com/codever/ticketup/service/ParticipantService.java
@@ -1,4 +1,16 @@
 package com.codever.ticketup.service;
 
+
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class ParticipantService {
+
+
+    private final ParticipantRepository participantRepo;
+
+
+
+
 }
