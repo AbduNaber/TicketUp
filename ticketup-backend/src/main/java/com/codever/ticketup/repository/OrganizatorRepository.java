3c32c0b883d9205ea2e86dd9257f8816022ea86d
ilkerymz <iilker0053@gmail.com>
2024-10-16 20:38:06 +0300

    Organizator repository created

---
+++ ticketup-backend/src/main/java/com/codever/ticketup/repository/OrganizatorRepository.java
@@ -0,0 +1,8 @@
+package com.codever.ticketup.repository;
+
+
+import com.codever.ticketup.model.Organizator;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganizatorRepository extends JpaRepository<Organizator, Long> {
+}
