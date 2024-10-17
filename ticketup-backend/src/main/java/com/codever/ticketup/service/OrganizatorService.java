bd8597d430f0ddb1921d07b67e74498c4ede0c90
Abdullah <aturkmen2021@gtu.edu.tr>
2024-10-17 23:09:54 +0300

    all CRUD operation is written to OrganizatorService , ParticipantService and TicketService .

--- ticketup-backend/src/main/java/com/codever/ticketup/service/OrganizatorService.java
+++ ticketup-backend/src/main/java/com/codever/ticketup/service/OrganizatorService.java
@@ -1,4 +1,54 @@
 package com.codever.ticketup.service;
 
+
+import com.codever.ticketup.model.Organizator;
+import com.codever.ticketup.repository.OrganizatorRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+@Getter
+@Setter
 public class OrganizatorService {
+
+    private final OrganizatorRepository organizatorRepository;
+
+
+    public List<Organizator> getAllOrganizators() {
+        return organizatorRepository.findAll();
+    }
+
+    public Organizator getOrganizatorById(Long id) {
+        return organizatorRepository.getReferenceById(id);
+    }
+    public Organizator updateOrganizator(Organizator organizator) {
+        return organizatorRepository.findById(organizator.getId()).map(
+                organizator1 -> {
+                    organizator1.setId(organizator.getId());
+                    organizator1.setUsername(organizator.getUsername());
+                    organizator1.setEmail(organizator.getEmail());
+                    organizator1.setPasswordHash(organizator.getPasswordHash());
+                    organizator1.setCreatedAt(organizator.getCreatedAt());
+                    return organizatorRepository.save(organizator1);
+
+                }
+        ).orElseThrow( () -> new RuntimeException(" Organizator not found with id: " + organizator.getId()));
+    }
+
+    public void deleteOrganizator(Long id) {
+        organizatorRepository.deleteById(id);
+    }
+    public void addOrganizator(Organizator organizator) {
+        organizatorRepository.save(organizator);
+    }
+
+
+
+
+
 }
