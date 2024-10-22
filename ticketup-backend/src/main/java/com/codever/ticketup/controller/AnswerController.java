8d9d9c0bc29e7d672f657816197dcba4bfc86506
Ahmet-Baha <ahmetbaha1275@gmail.com>
2024-10-22 22:38:38 +0300

    CRUD operations were written for the AnswerController.

--- ticketup-backend/src/main/java/com/codever/ticketup/controller/AnswerController.java
+++ ticketup-backend/src/main/java/com/codever/ticketup/controller/AnswerController.java
@@ -1,4 +1,42 @@
-package main.java.com.codever.ticketup.controller;
+package com.codever.ticketup.controller;
 
+import com.codever.ticketup.model.Answer;
+import com.codever.ticketup.service.AnswerService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("ticketup/answers")
 public class AnswerController {
+
+    private final AnswerService answerService;
+
+    @GetMapping(path = "/list")
+    public List<Answer> getAllAnswers() {
+        return answerService.getAllAnswers();
+    }
+
+    @GetMapping(path = "/list/{id}")
+    public Answer getAnswerById(@PathVariable(name = "id") Long id) {
+        return answerService.getAnswerById(id);
+    }
+
+    @PutMapping(path = "/update/{id}")
+    public Answer updateAnswer(@RequestBody Answer answer, @PathVariable(name = "id") Long id) {
+        return answerService.updateAnswer(id,answer);
+    }
+
+    @PostMapping(path = "/create")
+    public Answer createAnswer(@RequestBody Answer answer) {
+        return answerService.createAnswer(answer);
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public void deleteAnswer(@PathVariable(name = "id") Long id) {
+        answerService.deleteAnswer(id);
+    }
+
 }
