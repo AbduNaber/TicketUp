e20fe81dd88da1f638b67752f8e4ce26352d6512
Ahmet-Baha <ahmetbaha1275@gmail.com>
2024-10-22 22:52:33 +0300

    CRUD operations were written for the QuestionController.

--- ticketup-backend/src/main/java/com/codever/ticketup/controller/QuestionController.java
+++ ticketup-backend/src/main/java/com/codever/ticketup/controller/QuestionController.java
@@ -1,4 +1,41 @@
-package main.java.com.codever.ticketup.controller;
+package com.codever.ticketup.controller;
 
+import com.codever.ticketup.model.Question;
+import com.codever.ticketup.service.QuestionService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("ticketup/questions")
 public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @GetMapping(path = "/list")
+    public List<Question> getAllQuestions() {
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping(path = "/list/{id}")
+    public Question getQuestionById(@PathVariable Long id) {
+        return questionService.getQuestionById(id);
+    }
+
+    @PutMapping(path = "/update/{id}")
+    public Question updateQuestion(@PathVariable Long id, @RequestBody Question question) {
+        return questionService.updateQuestion(id, question);
+    }
+
+    @PostMapping(path = "/create")
+    public Question createQuestion(@RequestBody Question question) {
+        return questionService.createQuestion(question);
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public void deleteQuestion(@PathVariable Long id) {
+        questionService.deleteQuestion(id);
+    }
 }
