00694bca8cc43a5fa6b42b6c51a5d77335e2d799
Ahmet-Baha <ahmetbaha1275@gmail.com>
2024-10-22 22:42:54 +0300

    CRUD operations were written for the EventController.

--- ticketup-backend/src/main/java/com/codever/ticketup/controller/EventController.java
+++ ticketup-backend/src/main/java/com/codever/ticketup/controller/EventController.java
@@ -1,6 +1,42 @@
-package main.java.com.codever.ticketup.controller;
+package com.codever.ticketup.controller;
 
+
+import com.codever.ticketup.model.Event;
+import com.codever.ticketup.service.EventService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("ticketup/events")
 public class EventController {
 
+    private final EventService eventService;
 
+    @GetMapping(path = "/list")
+    public List<Event> getAllEvents() {
+        return eventService.getAllEvents();
+    }
+
+    @GetMapping(path = "/list/{id}")
+    public Event getEventById(@PathVariable Long id) {
+        return eventService.getEventById(id);
+    }
+
+    @PutMapping(path = "/update/{id}")
+    public Event updateEvent(@PathVariable Long id, @RequestBody Event event) {
+        return eventService.updateEvent(id, event);
+    }
+
+    @PostMapping(path = "/create")
+    public Event createEvent(@RequestBody Event event) {
+        return eventService.createEvent(event);
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public void deleteEvent(@PathVariable Long id) {
+        eventService.deleteEvent(id);
+    }
 }
