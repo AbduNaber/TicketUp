3940457cf9f3ca9423b715fedc7b543a04d42888
ilkerymz <iilker0053@gmail.com>
2024-10-26 11:00:44 +0300

    fix: @Autowired annotation deleted update method fixed

--- ticketup-backend/src/main/java/com/codever/ticketup/service/EventService.java
+++ ticketup-backend/src/main/java/com/codever/ticketup/service/EventService.java
@@ -2,16 +2,20 @@ package com.codever.ticketup.service;
 
 import com.codever.ticketup.model.Event;
 import com.codever.ticketup.repository.EventRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
+@Getter
+@Setter
 public class EventService {
 
-    @Autowired
-    private EventRepository eventRepository;
+    private final EventRepository eventRepository;
 
     public List<Event> getAllEvents() {
         return eventRepository.findAll();
@@ -22,15 +26,16 @@ public class EventService {
                 .orElseThrow(() -> new RuntimeException("Event with id " + id + " not found"));
     }
 
-    public Event updateEvent(Long id, Event event) {
-        Event existingEvent = eventRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Event with id " + id + " not found"));
-
-        existingEvent.setName(event.getName());
-        existingEvent.setDescription(event.getDescription());
-        existingEvent.setLocation(event.getLocation());
-        existingEvent.setEventDate(event.getEventDate());
-        return eventRepository.save(existingEvent);
+    public Event updateEvent(Event event) {
+        return eventRepository.findById(event.getId()).map(
+                event1 -> {
+                    event1.setName(event.getName());
+                    event1.setDescription(event.getDescription());
+                    event1.setLocation(event.getLocation());
+                    event1.setEventDate(event.getEventDate());
+                    return eventRepository.save(event1);
+                }
+        ).orElseThrow(() -> new RuntimeException("Event with id " + event.getId() + " not found"));
     }
 
     public void deleteEvent(Long id) {
