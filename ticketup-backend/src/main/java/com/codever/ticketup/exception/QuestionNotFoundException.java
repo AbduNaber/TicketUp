7a2d7cd358832184c30668544b37cd0855295ec7
ilkerymz <iilker0053@gmail.com>
2024-10-27 23:17:07 +0300

    feat: Add QuestionNotFoundException for missing questions

---
+++ ticketup-backend/src/main/java/com/codever/ticketup/exception/QuestionNotFoundException.java
@@ -0,0 +1,7 @@
+package com.codever.ticketup.exception;
+
+public class QuestionNotFoundException extends RuntimeException{
+    public QuestionNotFoundException(String message) {
+        super(message);
+    }
+}
