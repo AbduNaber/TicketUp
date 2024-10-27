0a9ccb9542b8354bb8fc8552bd3b7223e22c48f8
ilkerymz <iilker0053@gmail.com>
2024-10-27 23:16:20 +0300

    feat: Add OrganizatorNotFoundException for missing organizators

---
+++ ticketup-backend/src/main/java/com/codever/ticketup/exception/OrganizatorNotFoundException.java
@@ -0,0 +1,7 @@
+package com.codever.ticketup.exception;
+
+public class OrganizatorNotFoundException extends RuntimeException {
+    public OrganizatorNotFoundException(String message) {
+        super(message);
+    }
+}
