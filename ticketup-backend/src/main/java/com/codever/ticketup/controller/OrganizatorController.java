33556fd4957d383d10abcb50007e84e31d3dd83d
ilkerymz <iilker0053@gmail.com>
2024-11-10 11:20:38 +0300

    register and login methods added

--- ticketup-backend/src/main/java/com/codever/ticketup/controller/OrganizatorController.java
+++ ticketup-backend/src/main/java/com/codever/ticketup/controller/OrganizatorController.java
@@ -1,9 +1,12 @@
 package com.codever.ticketup.controller;
 
+import com.codever.ticketup.dto.LoginRequestDto;
 import com.codever.ticketup.model.Organizator;
 import com.codever.ticketup.service.OrganizatorService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
@@ -14,15 +17,25 @@ public class OrganizatorController {
 
     private final OrganizatorService organizatorService;
 
-    @PostMapping(path = "/create")
-    public void createOrganizator(@RequestBody Organizator organizator) {
-        organizatorService.addOrganizator(organizator);
+    @PostMapping(path = "/register")
+    public ResponseEntity<String> register(@RequestBody Organizator organizator) {
+        organizatorService.register(organizator);
+        return ResponseEntity.ok("Organizator registered");
+    }
+    @PostMapping(path = "/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequestDto loginRequestDto) {
+        System.out.println("Controller email " + loginRequestDto.getEmail());
+        System.out.println("Controller password " + loginRequestDto.getPassword());
+        String token = organizatorService.login(loginRequestDto.getEmail(), loginRequestDto.getPassword());
+        System.out.println("Login called");
+        return ResponseEntity.ok(token);
     }
 
     @GetMapping(path = "/list")
     public List<Organizator> getAllOrganizators() {
-
+        System.out.println("List called");
         return organizatorService.getAllOrganizators();
+
     }
 
     @GetMapping(path = "/list/{id}")
@@ -39,5 +52,6 @@ public class OrganizatorController {
     public void deleteOrganizator(@PathVariable(name = "id") Long id) {
 
         organizatorService.deleteOrganizator(id);
+        System.out.println("Delete called");
     }
 }
