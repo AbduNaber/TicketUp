ab38e02b297cf5b29a6bc7de825e5b09e94c892c
ilkerymz <iilker0053@gmail.com>
2024-11-10 11:24:09 +0300

    token provider created

---
+++ ticketup-backend/src/main/java/com/codever/ticketup/security/JwtTokenProvider.java
@@ -0,0 +1,30 @@
+package com.codever.ticketup.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private Long expirationTime;
+
+    public String createToken(String email) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expirationTime);
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS256, secretKey.getBytes()) // 0.9.1 sürümünde Key yerine byte[] kullanılır
+                .compact();
+    }
+}
