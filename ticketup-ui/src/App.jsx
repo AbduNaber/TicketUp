8eff6b988e34a406619b67e5d7dec4a0928f4ca9
yusufaslanargun <106274485+yusufaslanargun@users.noreply.github.com>
2024-11-29 22:13:56 +0300

    event page added

--- ticketup-ui/src/App.jsx
+++ ticketup-ui/src/App.jsx
@@ -4,6 +4,7 @@ import Login from './login';
 import SignupForm from './register'; 
 import Form from './form';
 import ExamplePage from './ExamplePage';
+import EventPage from './event';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignupForm />} />
         <Route path="/form" element={<Form />} />
+        <Route path="/event" element={<EventPage />} />
       </Routes>
     </Router>
   );
