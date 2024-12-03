a1c2f6565688dbd0916f7cdc872897beb3f63dd8
ilkerymz <iilker0053@gmail.com>
2024-12-03 22:49:49 +0300

    Events fetched to organizator page from backend

--- ticketup-ui/src/App.jsx
+++ ticketup-ui/src/App.jsx
@@ -4,20 +4,21 @@ import Login from './login';
 import SignupForm from './register'; 
 import Form from './form';
 import ExamplePage from './ExamplePage';
-import EventPage from './event';
+import Preview from './preview';
 import AdminPanel from './AdminPanel';
+import Event from './event';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/organizator" element={<ExamplePage />} />
+        <Route path="/organizator" element={<AdminPanel></AdminPanel>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignupForm />} />
         <Route path="/form" element={<Form />} />
-        <Route path="/event" element={<EventPage />} />
-        <Route path="/adminpanel" element={<AdminPanel />} />
+        <Route path="/preview" element={<Preview/>} />
+        <Route path="/event/:id" element={<Event></Event>} />
       </Routes>
     </Router>
   );
