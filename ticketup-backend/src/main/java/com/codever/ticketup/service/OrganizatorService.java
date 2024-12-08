9afeb79b40394442e8ad6ef38d21f29dbe89ff5b
ilkerymz <iilker0053@gmail.com>
2024-12-08 16:01:27 +0300

    Methods updated using DTO

--- ticketup-backend/src/main/java/com/codever/ticketup/service/OrganizatorService.java
+++ ticketup-backend/src/main/java/com/codever/ticketup/service/OrganizatorService.java
@@ -1,6 +1,8 @@
 package com.codever.ticketup.service;
 
 
+import com.codever.ticketup.dto.organizator.OrganizatorDto;
+import com.codever.ticketup.dto.organizator.OrganizatorDtoIU;
 import com.codever.ticketup.model.Organizator;
 import com.codever.ticketup.repository.OrganizatorRepository;
 import com.codever.ticketup.security.JwtService;
@@ -8,6 +10,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.BeanUtils;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,6 +19,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -31,14 +35,16 @@ public class OrganizatorService {
 
     private final AuthenticationManager authenticationManager;
 
-    public void register(@NotNull Organizator organizator) {
-
-        if(organizatorRepository.findByEmail(organizator.getEmail()) != null) {
-           throw new RuntimeException("Email already in use");
+    public void register(@NotNull OrganizatorDtoIU organizatorDtoIU) {
+        if(organizatorRepository.findByEmail(organizatorDtoIU.getEmail()) != null) {
+            throw new RuntimeException("Email already exists");
         }
-        organizator.setPasswordHash(encoder.encode(organizator.getPasswordHash()));
 
+        Organizator organizator = new Organizator();
+        BeanUtils.copyProperties(organizatorDtoIU, organizator);
+        organizator.setPasswordHash(encoder.encode(organizatorDtoIU.getPasswordHash()));
         organizatorRepository.save(organizator);
+
     }
 
     public String login(String email, String password) {
@@ -52,27 +58,38 @@ public class OrganizatorService {
 
     }
 
-    public List<Organizator> getAllOrganizators() {
-        return organizatorRepository.findAll();
+    public List<OrganizatorDto> getAllOrganizators() {
+        List<Organizator> organizators = organizatorRepository.findAll();
+
+        return organizators.stream().map(organizator -> {
+            OrganizatorDto organizatorDto = new OrganizatorDto();
+            BeanUtils.copyProperties(organizator, organizatorDto);
+            return organizatorDto;
+        }).collect(Collectors.toList());
     }
 
-    public Organizator getOrganizatorById(UUID id) {
+    public OrganizatorDto getOrganizatorById(UUID id) {
         Organizator organizator = organizatorRepository.getOrganizatorsById(id);
         if(organizator == null) {
             throw new RuntimeException("Organizator not found");
         }
-        return organizator;
+        OrganizatorDto organizatorDto = new OrganizatorDto();
+        BeanUtils.copyProperties(organizator, organizatorDto);
+        return organizatorDto;
     }
 
-    public Organizator getOrganizatorByEmail(String email) {
+    public OrganizatorDto getOrganizatorByEmail(String email) {
         if(organizatorRepository.findByEmail(email) == null) {
             throw new RuntimeException("Organizator not found");
         }
-
-        return organizatorRepository.findByEmail(email);
-
+        Organizator organizator = organizatorRepository.findByEmail(email);
+        OrganizatorDto organizatorDto = new OrganizatorDto();
+        BeanUtils.copyProperties(organizator, organizatorDto);
+        return organizatorDto;
     }
 
+
+    //TODO: Change this method with DTO
     public Organizator updateOrganizator(Organizator organizator) {
         return organizatorRepository.findById(organizator.getId()).map(
                 organizator1 -> {
