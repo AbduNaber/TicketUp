5d1c7dc88d5bd726a4e2b7285f122638511b4bf5
ilkerymz <iilker0053@gmail.com>
2024-12-08 16:01:08 +0300

    Organizator Dto's created

---
+++ ticketup-backend/src/main/java/com/codever/ticketup/dto/organizator/OrganizatorDtoIU.java
@@ -0,0 +1,20 @@
+package com.codever.ticketup.dto.organizator;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrganizatorDtoIU {
+
+    private String name;
+    private String surname;
+    private String organizationName;
+    private String email;
+    private String passwordHash;
+}
