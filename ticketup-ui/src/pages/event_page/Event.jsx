1d834cf1ad47e0e5d7812001b9c7438fd9e0edb2
ilkerymz <iilker0053@gmail.com>
2024-12-29 21:40:09 +0300

    Some possible bugs prevented

--- ticketup-ui/src/pages/event_page/Event.jsx
+++ ticketup-ui/src/pages/event_page/Event.jsx
@@ -4,6 +4,7 @@ import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import TopBar from "../../components/TopBar";
 
 const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
 
@@ -23,7 +24,7 @@ const Event = () => {
 
   const handleGoToForm = () => {
     console.log("res");
-    navigate("/form", { state: { eventID: id, imageLink: event?.imgUrl } });
+    navigate("/form", { state: { eventID: id, imageLink: event?.imgUrl, eventName: event?.name } });
   };
 
   const formatDateForGoogle = (date, time) => {
@@ -118,24 +119,7 @@ const Event = () => {
   return (
       <div className="flex flex-col min-h-screen bg-white overflow-y-auto">
         {/* Top Bar */}
-        <div className="flex justify-between items-center px-5 py-2 h-[10vh] text-white bg-white">
-          <div className="flex-1">
-            <a href="https://www.example.com">
-              <img src="/src/assets/icons/ticketup_icon_full.png" alt="Logo" className="w-[200px]" />
-            </a>
-          </div>
-          <div className="flex justify-center items-center ">
-            <a href="#" className="text-black text-sm mx-6 hover:underline">Ana Sayfa</a>
-            <a href="#" className="text-black text-sm mx-6 hover:underline">Neden Biz</a>
-            <a href="#" className="text-black text-sm mx-6 hover:underline">Bizimle İletişime Geçin</a>
-          </div>
-          <div className="flex items-center gap-4 flex-1 justify-end">
-            <GradientButton text="Biletini Sorgula" onClick={() => {}} />
-            <a href="https://www.example.com">
-              <img src="/src/assets/icons/account.png" alt="Logo" className="w-10 h-10 rounded-full" />
-            </a>
-          </div>
-        </div>
+        <TopBar></TopBar>
 
         {/* Content */}
         <div className="flex-grow mx-[6vw] mt-[7.5vh] mb-[30vh] flex flex-col items-center">
