67fa1ec642ee898de05b6c74499b6e4f62f143c2
ilkerymz <iilker0053@gmail.com>
2024-12-30 17:09:05 +0300

    Ticket page and TopBar improved

--- ticketup-ui/src/components/TopBar.jsx
+++ ticketup-ui/src/components/TopBar.jsx
@@ -4,13 +4,23 @@ import { Link, useNavigate } from "react-router-dom";
 const TopBar = () => {
 
   const navigate = useNavigate();
+  const token = sessionStorage.getItem("token");
 
   const goToTicketQuery = () => {
     navigate("/query-ticket");
   }
+
+  const handleGoToAccount = () => {
+    console.log("Go To Profile");
+    if(token){
+      navigate("/organizer");
+    }else {
+      navigate("/login");
+    }
+  }
   return (
-    <div className="w-full bg-white top-0 left-0 z-50 shadow">
-      <div className="flex justify-between items-center px-5 py-2 h-[10vh] text-white bg-white">
+    <div className="w-full bg-gray-800 top-0 left-0 z-50 shadow">
+      <div className="flex justify-between items-center px-5 py-2 h-[10vh] text-white">
         {/* Logo */}
         <div className="flex-1">
           <a href="https://www.example.com">
@@ -23,22 +33,22 @@ const TopBar = () => {
         </div>
         {/* Navigation Links */}
         <div className="flex justify-center items-center">
-          <Link to="#" className="text-black text-sm mx-6 hover:underline">
+          <Link to="#" className="text-white text-sm mx-6 hover:underline">
             Ana Sayfa
           </Link>
-          <Link to="/why-us" className="text-black text-sm mx-6 hover:underline">
+          <Link to="/why-us" className="text-white text-sm mx-6 hover:underline">
             Neden Biz
           </Link>
-          <Link to="/contact-ticketup" className="text-black text-sm mx-6 hover:underline">
+          <Link to="/contact-ticketup" className="text-white text-sm mx-6 hover:underline">
             Bizimle İletişime Geçin
           </Link>
         </div>
         {/* Profile and Button */}
         <div className="flex items-center gap-4 flex-1 justify-end">
           <GradientButton text="Biletini Sorgula" onClick={goToTicketQuery} />
-          <a href="https://www.example.com">
+          <a onClick={handleGoToAccount}>
             <img
-              src="/src/assets/icons/account.png"
+              src="/src/assets/icons/profile_wh_icon.svg"
               alt="Logo"
               className="w-10 h-10 rounded-full"
             />
