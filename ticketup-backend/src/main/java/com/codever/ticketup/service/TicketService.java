3ac1843d93d2703b082cb8aafa692404bcd49e66
Ahmet-Baha <ahmetbaha1275@gmail.com>
2025-01-02 16:56:35 +0300

    Ticket inquiry button has been activated.

--- ticketup-backend/src/main/java/com/codever/ticketup/service/TicketService.java
+++ ticketup-backend/src/main/java/com/codever/ticketup/service/TicketService.java
@@ -1,6 +1,8 @@
 package com.codever.ticketup.service;
 
+import com.codever.ticketup.model.Participant;
 import com.codever.ticketup.model.Ticket;
+import com.codever.ticketup.repository.ParticipantRepository;
 import com.codever.ticketup.repository.TicketRepository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,6 +11,7 @@ import lombok.Setter;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -18,6 +21,7 @@ import java.util.UUID;
 public class TicketService {
 
     private final TicketRepository ticketRepository;
+    private final ParticipantRepository participantRepository;
 
     public List<Ticket> getAllTickets(){
         return ticketRepository.findAll();
@@ -44,6 +48,19 @@ public class TicketService {
     public UUID addTicket(Ticket ticket){
         ticketRepository.save(ticket);
         return ticket.getId();
+    }
+
+    public boolean isParticipantWithEmailOrPhone(String email, String phone, UUID participantId) {
+        // Participant tablosunda e-posta veya telefon numarası ve ID eşleşmesini kontrol et
+        Optional<Participant> participantOpt = participantRepository.findById(participantId);
+        return participantOpt.map(participant -> {
+            if (email != null && !email.isEmpty()) {
+                return participant.getEmail().equalsIgnoreCase(email);
+            } else if (phone != null && !phone.isEmpty()) {
+                return participant.getPhone().equals(phone); // Telefon eşleşmesi
+            }
+            return false; // Ne email ne telefon girilmişse eşleşme başarısız
+        }).orElse(false);
     }
 
 }
