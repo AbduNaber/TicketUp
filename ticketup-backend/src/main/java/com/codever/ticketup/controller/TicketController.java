81419394115c3c08fd050fd28ce6f20b73656078
Ahmet-Baha <ahmetbaha1275@gmail.com>
2025-01-04 04:11:15 +0300

    For test

--- ticketup-backend/src/main/java/com/codever/ticketup/controller/TicketController.java
+++ ticketup-backend/src/main/java/com/codever/ticketup/controller/TicketController.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.UUID;
 // Bu satır test için
 // Bu satır test için
+// Bu satır test için
 @AllArgsConstructor
 @RestController
 @RequestMapping("ticketup/tickets")
