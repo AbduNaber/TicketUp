e8a5cd388533e4a0a1d470368dc974f3a3e38a77
ilkerymz <iilker0053@gmail.com>
2025-01-17 14:25:16 +0300

    Cancel button removed

--- ticketup-ui/src/pages/organiser_page/EditOrganizator.jsx
+++ ticketup-ui/src/pages/organiser_page/EditOrganizator.jsx
@@ -216,10 +216,7 @@ const EditOrganizator = ({setActiveItem}) => {
 
         {/* Action Buttons */}
         <div className="flex justify-end space-x-3 pt-4">
-        <Button variant="outline" onClick={() => setActiveItem(1)}>
-  İptal
-</Button>
-          <Button className="space-x-2" onClick={handleSaveChanges}>
+        <Button className="space-x-2" onClick={handleSaveChanges}>
             <Save className="h-4 w-4" />
             <span>Değişikleri Kaydet</span>
           </Button>
