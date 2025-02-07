1170ab224e4a89093048f32d6ee9fad77675d26d
ilkerymz <iilker0053@gmail.com>
2025-02-07 14:02:56 +0300

    Organizator table connected with event table

--- ticketup-backend/src/main/java/com/codever/ticketup/dto/organizator/OrganizatorDto.java
+++ ticketup-backend/src/main/java/com/codever/ticketup/dto/organizator/OrganizatorDto.java
@@ -1,9 +1,12 @@
 package com.codever.ticketup.dto.organizator;
 
+import com.codever.ticketup.model.Event;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -15,4 +18,5 @@ public class OrganizatorDto {
     private String organizationName;
     private String email;
     private String profilePicture;
+    private List<Event> events;
 }
